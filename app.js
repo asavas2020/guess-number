49f1e98041d3c4419a692df30fa3f7d07ba33481
Gokhan <gknsvsdata@gmail.com>
2022-06-06 22:32:56 +0300

    version2

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
 
-
-let number = 30 //Math.floor(Math.random()*(100) + 1);
-// console.log(number);
-
-// let startGame = document.getElementById("#guess-h2");
+let start = document.querySelector(".start");
 let input = document.querySelector(".input1");
 let button = document.querySelector(".btn");
 let result = document.getElementById("guess-p");
-// let attemt = document.querySelector("p");
+let attemt = document.querySelector(".span");
+let win = document.querySelector(".win")
+let up = document.querySelector(".up")
+let down = document.querySelector(".down")
 
- 
 
+let number = Math.floor(Math.random()*(100) + 1);
+// console.log(number);
 
+live = 0   
 
 const check = function() {
-
-    while (true) {
-   
-    live = 0    
-    if(input.value>number){
-        result.innerHTML = `Sorry You must enter a number less than ${input.value}, please try again`;
+  
+  if(input.value>number){
+        result.innerHTML = `Sorry You must enter a number less than ${input.value} ⬇⬇⬇⬇ please try again...`;
+        input.value = "" ;
+        down.play() ;
+          
     }
       
-    else if(input.value<number){
-        result.innerHTML = `Sorry You must enter a number greater than ${input.value}, please try again`;
-        
+   else if(input.value<number){
+        result.innerHTML = `Sorry You must enter a number greater than ${input.value} ⬆⬆⬆⬆ please try again...`;
+        input.value = ""
+        up.play() ;
+
     } else{
-        result.innerHTML = 'Congratulation You Win!!!';
-    
-    }break
-  }
+        result.innerHTML = '🚀🚀🚀Congratulation You Win 🚀🚀🚀!!!'
+        input.value = ""
+        win.play() ;
+        }
+
+     live+=1;
+    attemt.innerHTML = live;
+
  } 
 
 button.addEventListener("click", check)
 
-button.addEventListener("keydown", (e) => {
-   if(e.code == "13") {
-       check()
-   }
+start.addEventListener("click",() => {
+    location.reload()
 })
+
+
+input.addEventListener("keydown",(e=>{
+    if(e.keyCode===13){
+      check()
+    }
+  }))
