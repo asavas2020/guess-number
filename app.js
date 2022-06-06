c3ddf901199914b2bb5f52010f0f9854bb1d64f7
Gokhan <gknsvsdata@gmail.com>
2022-06-06 19:52:51 +0300

    version1

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 
 
 let number = 30 //Math.floor(Math.random()*(100) + 1);
-console.log(number);
+// console.log(number);
 
 // let startGame = document.getElementById("#guess-h2");
-let input = document.querySelector("type");
-let button = document.querySelector("button");
-let result = document.getElementById("#guess-h3");
+let input = document.querySelector(".input1");
+let button = document.querySelector(".btn");
+let result = document.getElementById("guess-p");
 // let attemt = document.querySelector("p");
 
  
 
-   
-let guess = +prompt("Enter A number (1 - 100)");
-   input.innerHTML = guess
 
 
-button.addEventListener("click", check) 
-
-function check() {
+const check = function() {
 
     while (true) {
    
-        
-    if(guess>number){
-        result.innerText = console.log(`Sorry You must enter a number less than ${guess}, please try again`);
+    live = 0    
+    if(input.value>number){
+        result.innerHTML = `Sorry You must enter a number less than ${input.value}, please try again`;
     }
       
-    else if(guess<number){
-        result.innerText = console.log(`Sorry You must enter a number greater than ${guess}, please try again`);
+    else if(input.value<number){
+        result.innerHTML = `Sorry You must enter a number greater than ${input.value}, please try again`;
         
     } else{
-        result.innerText = console.log("Congratulation You Win!!!");
-    break
-    }
+        result.innerHTML = 'Congratulation You Win!!!';
+    
+    }break
   }
  } 
- check()
 
+button.addEventListener("click", check)
 
-
+button.addEventListener("keydown", (e) => {
+   if(e.code == "13") {
+       check()
+   }
+})
